bac22251f892cdec53f477af9ec7447be26602ab
JosephManjappilly <josjoy94@gmail.com>
2025-06-17 12:30:38 +0200

    preped for move base- maze room too small

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/joseph/slam_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/joseph/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/joseph/catkin_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/joseph/slam_ws/devel;/home/joseph/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/joseph/slam_ws/devel;/home/joseph/catkin_ws/devel;/opt/ros/noetic' != '' else []
